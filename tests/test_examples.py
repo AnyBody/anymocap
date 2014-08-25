bdc15bc5ec48858b50749825d77a1151ca75b98f
Morten Enemark Lund <melund@gmail.com>
2014-08-25 15:23:49 +0200

    Update test scripts

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -9,8 +9,10 @@ from anypytools.generate_macros import MacroGenerator
 modelfolder = os.path.join(os.path.dirname(__file__),'..','Model')
 examplefolder = os.path.join(os.path.dirname(__file__),'..','Examples')
 
+
+
 @pytest.yield_fixture()
-def init_anymocap(tmpdir, scope="module"):
+def temp_dir(tmpdir, scope="module"):
     """ Copies the AnyMocap model to temp directory to run tests"""
     shutil.copytree(modelfolder, str( tmpdir.join('Model') ) )
     shutil.copytree(examplefolder, str( tmpdir.join('Examples') ) )
@@ -18,24 +20,44 @@ def init_anymocap(tmpdir, scope="module"):
         yield tmpdir
 
 
-def test_example_DynamicOnly(init_anymocap):
+
+def test_example_plugingait(temp_dir):
     app =  AnyPyProcess() 
     mg = MacroGenerator()
-    defines = {'JOINT_AND_DRIVERS_OPTIMIZED',0}
-    mg.add_load('Examples/DynamicOnly/Main.any',define_kw = defines)
-    mg.add_run_operation('Main.RunApplication.RunParameterIdentificaiton')
-    mg.add_run_operation('Main.RunApplication.SaveParameters')
-    result = app.start_macro(macro)
-    defines = {'JOINT_AND_DRIVERS_OPTIMIZED',1}
-    mg.add_load('Examples/DynamicOnly/Main.any',define_kw = defines)
-    mg.add_run_operation('Main.RunApplication.LoadParameters')
-    mg.add_run_operation('Main.RunApplication.RunInverseDynamics')
-    #result = app.start_macro(macro)
+    defines = {'JOINT_AND_DRIVERS_OPTIMIZED':0}
+    mg.add_load('Examples/Plug-in-gait/Main.any',define_kw = defines)
+#    mg.add_run_operation('Main.Studies.ParameterIdentification.ParameterOptimization')
+#    mg.add_save_design('Main.Studies.ParameterIdentification',
+#                       str(temp_dir.join('paramter.txt'))  )
+    result = app.start_macro( mg.generate_macros() )
+    assert 'ERROR' not in result[0], result[0]['ERROR']
     
-    assert 'ERROR' not in result[0]
+#    defines = {'JOINT_AND_DRIVERS_OPTIMIZED',1}
+#    mg.add_load('Examples/Plug-in-gait/Main.any',define_kw = defines)
+#    mg.add_run_operation('Main.RunApplication.LoadParameters')
+#    mg.add_run_operation('Main.RunApplication.RunInverseDynamics')
+#    result = app.start_macro( mg.generate_macros() )
+#    assert 'ERROR' not in result[0]
     
-def test_example_InitialPosExample(init_anymocap):
+    
+def test_example_InitialPosExample(temp_dir):
     app =  AnyPyProcess() 
     macro = [['load "Examples/InitialPosExample/Main.any"']]
     result = app.start_macro(macro)
-    assert 'ERROR' not in result[0]
+    assert 'ERROR' not in result[0], result[0]['ERROR']
+
+
+def test_example_MarkerWeightExample(temp_dir):
+    app =  AnyPyProcess() 
+    macro = [['load "Examples/MarkerWeightExample/Main.any"']]
+    result = app.start_macro(macro)
+    assert 'ERROR' not in result[0], result[0]['ERROR']
+
+def test_example_LTHT(temp_dir):
+    app =  AnyPyProcess() 
+    macro = [['load "Examples/LTHT/Subject_1/Static/main.any"'],
+             ['load "Examples/LTHT/Subject_1/Walk/main.any"']]
+    results = app.start_macro(macro)
+    for result in results:
+        assert 'ERROR' not in result, result['ERROR']
+
