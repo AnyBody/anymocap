dc722b15c02eaaa42c52bb2019a16fd8a8449eb1
Morten Enemark Lund <melund@gmail.com>
2014-09-11 10:00:22 +0200

    TestSetup. Added AnyMocap related stuff to the local conftest file, and added a pytest.ini file

--- conftest.py
+++ conftest.py
@@ -1,2 +1,17 @@
+import os
+import pytest
 pytest_plugins = "anypytools"
+        
 
+@pytest.fixture()
+def anytest(anytest): 
+    """This fixture 'overloads' the anytest fixture
+    and injects the some extra path and define 
+    statements which are specific to the AnyMocapModel.
+    """
+    anymocap_path = os.path.join(os.path.dirname(__file__),
+                                'Model', 'AnyMocapModel.any')
+                                
+    anytest.path_kw.update( {'ANYMOCAP':anymocap_path} )
+    anytest.define_kw.update( {'JOINT_AND_DRIVERS_OPTIMIZED':0} )
+    return anytest
